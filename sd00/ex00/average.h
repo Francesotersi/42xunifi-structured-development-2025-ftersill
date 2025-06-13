1e75796edd263a8ccf76dda148ecf38128714281
Francesco Tersillo <ftersill@c1r3p6.42firenze.it>
2025-06-13 08:22:47 +0200

    meow

---
+++ sd00/ex00/average.h
@@ -0,0 +1,8 @@
+#ifndef AVARAGE_H
+# define AVARAGE_H
+
+#include <stdio.h>
+
+float average(const int *arr, int size);
+
+#endif
---
+++ sd00/ex06/grade_mapping.c
@@ -0,0 +1,6 @@
+#include "grade_mapping.h"
+
+void map_scores(const int *scores, int size, GradeMapper mapper, const char **mapped_grades)
+{
+    if mapper
+}
